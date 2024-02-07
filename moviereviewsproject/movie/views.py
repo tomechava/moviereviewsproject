a12b764a48588ab233b5342cd8489f08a448d63b
tomechava <techavarrg@eafit.edu.co>
2024-02-07 10:06:26 -0500

    cambios en la funcion home

--- moviereviewsproject/movie/views.py
+++ moviereviewsproject/movie/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
-    return render(request, 'home.html', {'name': 'Greg Lim'})
+    return render(request, 'home.html', {'name': 'Tomas Echavarria'})
 
 def about(request):
     #return HttpResponse('<h1>About Us</h1>')
